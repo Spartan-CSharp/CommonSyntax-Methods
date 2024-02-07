3480b56d2c5f5c6d8b459a81c2d82af2ccad1e92
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:32:39 -0500

    Create Code
    Create a welcome method, a method to ask for the user's name, and another to tell that user "Hello __".

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			WelcomeUser();
+			string name = GetUserName();
+			SayHello(name);
+			_ = Console.ReadLine();
+		}
+
+		private static void WelcomeUser()
+		{
+			Console.WriteLine("Welcome to Foundation in C#!");
+		}
+
+		private static string GetUserName()
+		{
+			Console.Write("What is your first name: ");
+			string output = Console.ReadLine();
+			return output;
+		}
+
+		private static void SayHello(string firstName)
+		{
+			Console.WriteLine($"Hello {firstName}!");
 		}
 	}
 }
